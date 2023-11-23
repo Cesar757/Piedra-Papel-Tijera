c99efa3d89e98178328578647da4d33313ca31b0
cesar <cesardotaplus2000@gmail.com>
2023-11-22 19:49:37 -0500

    Texto de la seleccion de la computadora

--- piedra.py
+++ piedra.py
@@ -4,3 +4,10 @@ print("bienvenidos al juego de piedra papel op tijera")
 print("ingrese un valor  piedra=3, tijera=2, papel=1")
 n=input()
 m=random.randint(1,3)
+if m==1:
+  c="papel"
+elif m==2:
+  c="tijera"
+else:
+  c="piedra"
+print(c)
