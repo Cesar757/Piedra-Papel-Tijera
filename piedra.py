146a6688a669467eef701370a7ed746e06ff47b7
cesar <cesardotaplus2000@gmail.com>
2023-11-22 20:11:52 -0500

    Desicion de ganador

--- piedra.py
+++ piedra.py
@@ -2,7 +2,7 @@ import random
 
 print("bienvenidos al juego de piedra papel op tijera")
 print("ingrese un valor  piedra=3, tijera=2, papel=1")
-n=input()
+n=int(input())
 m=random.randint(1,3)
 if m==1:
   c="papel"
@@ -10,4 +10,20 @@ elif m==2:
   c="tijera"
 else:
   c="piedra"
-print(c)
+
+if n==2 and m==2:
+  print("empate pc saco "+ c)
+else:
+  p=n+m
+  if p==4:
+    if n==1:
+      print("ganaste pc saco "+ c)
+    else:
+      print("perdiste pc saco "+ c)
+  else:
+    if n>m:
+      print("ganaste pc saco "+ c)
+    elif n==m:
+      print("empate pc saco "+ c)
+    else:
+      print("perdiste pc saco "+ c)
